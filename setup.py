0d5f1db14a61cd5b84c2737f92821f9763c6e5c6
Sudhir Pratap Yadav <yadav.1@iitj.ac.in>
2024-09-08 22:37:36 +0530

    gazebo simulation working

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'open_manipulator_gazebo'
 
@@ -10,12 +12,17 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro')),
+        (os.path.join('share', package_name, 'world'), glob('worlds/*.world')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='sudhir',
-    maintainer_email='yadav.1@iitj.ac.in',
-    description='TODO: Package description',
+    maintainer_email='sudhirpratapyadav@gmail.com',
+    description='Gazebo Simulation of Open manipulator arm in ROS2',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
